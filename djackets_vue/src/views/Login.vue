42a8e2a035ec6b66d7a7652625309b2a3d667bc8
principalkelvoe <you@example.com>
2021-11-09 21:34:54 +0300

    login

---
+++ djackets_vue/src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <section id="cover" class="min-vh-100">
+    <div id="cover-caption">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-4 is-offset-4">
+            <h1 class="title">Login</h1>
+            <form>
+              <div class="field">
+                <label>Email</label>
+                <div class="conrol">
+                  <input type="email" name="email" class="input" placeholder="Email" />
+                </div>
+              </div>
+              <div class="field">
+                <label>Password</label>
+                <div class="conrol">
+                  <input type="password" name="password" class="input" placeholder="Password" />
+                </div>
+              </div>
+              <div class="field">
+                <div class="conrol">
+                  <button class="button is-success">Submit</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
