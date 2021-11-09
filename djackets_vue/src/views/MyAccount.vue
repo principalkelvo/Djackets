7357b15bc1db52113022b33cc59fbff901030f66
principalkelvoe <you@example.com>
2021-11-09 21:35:15 +0300

    profile

---
+++ djackets_vue/src/views/MyAccount.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+      <div class="columns is-multiline">
+          <div class="column is-12">
+              <h1 class="title">My Profile</h1>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'MyAccount'
+}
+</script>
+
+<style>
+
+</style>
