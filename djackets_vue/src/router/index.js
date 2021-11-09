71b521f5693a3ad339d72014725724ab9ea3095b
principalkelvoe <you@example.com>
2021-11-09 21:34:41 +0300

    routes for login

--- djackets_vue/src/router/index.js
+++ djackets_vue/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import SignUp from '../views/Signup.vue'
+import Login from '../views/Login.vue'
+import MyAccount from '../views/MyAccount.vue'
 
 const routes = [
   {
@@ -14,6 +17,21 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/log-in',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/myaccount',
+    name: 'MyAccount',
+    component: MyAccount
   }
 ]
 
