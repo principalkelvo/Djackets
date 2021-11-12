1114b582c655f29f4ce90cf7c2c5aa05a5e06090
principalkelvoe <you@example.com>
2021-11-12 16:26:28 +0300

    cleaned home page

--- djackets_vue/src/views/Home.vue
+++ djackets_vue/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container">
+      <div class="columns is-multiline">
+          <div class="column is-12">
+              <h1 class="title">Home</h1>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
