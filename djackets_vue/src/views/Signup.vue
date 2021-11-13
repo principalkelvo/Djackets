83858ac51ab4d55d5d8da90340c6d323dadc60a1
principalkelvoe <you@example.com>
2021-11-14 00:42:06 +0300

    edited

--- djackets_vue/src/views/Signup.vue
+++ djackets_vue/src/views/Signup.vue
@@ -1,28 +1,31 @@
 <template>
-  <section id="cover" class="min-vh-100">
-    <div id="cover-caption">
+  <section id="cover" class="hero">
+    <div id="cover-caption" class="hero-body">
       <div class="container">
-        <div class="columns">
+        <div class="columns has-text-white">
           <div class="column is-4 is-offset-4">
-            <h1 class="title">Sign up</h1>
-            <form>
+            <h1 class="title has-text-white">Sign up</h1>
+            <form @submit.prevent="submitForm" class="is-justify-content-center">
               <div class="field">
                 <label>Email</label>
                 <div class="conrol">
-                  <input type="email" name="email" class="input" placeholder="Email" />
+                  <input type="email" name="email" class="input" placeholder="Email" v-model="username"/>
                 </div>
               </div>
               <div class="field">
                 <label>Password</label>
                 <div class="conrol">
-                  <input type="password" name="password" class="input" placeholder="Password" />
+                  <input type="password" name="password1" class="input" placeholder="Password" v-model="password1"/>
                 </div>
               </div>
               <div class="field">
                 <label>Confirm password</label>
                 <div class="conrol">
-                  <input type="password" name="password1" class="input" placeholder="Confirm Password" />
+                  <input type="password" name="password2" class="input" placeholder="Confirm Password" v-model="password2"/>
                 </div>
+              </div>
+              <div class="notification is-danger" v-if="errors.length">
+                <p v-for="error in errors" v-bind:key="error">{{error}}</p>
               </div>
               <div class="field">
                 <div class="conrol">
@@ -38,8 +41,64 @@
 </template>
 
 <script>
+import axios from 'axios'
+import {toast} from 'bulma-toast'
+
 export default {
   name: "SignUp",
+  data(){
+    return{
+      username:'',
+      password1 :'',
+      password2:'',
+      errors:[]
+      }
+  },
+  methods:{
+    submitForm(){
+      this.errors=[]
+      if(this.username===''){
+        this.errors.push('The username is missing')
+      }
+      if(this.password1===''||this.password1.length<4){
+        this.errors.push('The password is too short')
+      }
+      if(this.password1!==this.password2){
+        this.errors.push('The passwords are not matching')
+      }
+      if(!this.errors.length){
+        const formData={
+          username:this.username,
+          password:this.password1
+        }
+        axios
+          .post('/api/v1/users/',formData)
+          .then(response=>{
+            toast({
+              message:'Account was created, Please login',
+              type:'is-success',
+              dismissible:true,
+              pauseOnHover:true,
+              duration:2000,
+              position:'bottom-right'
+            })
+            this.$router.push('/log-in')
+          })
+          .catch(error=>{
+            if(error.response){
+              for(const property in error.response.data){
+                this.errors.push(`${property}:${error.response.data[property]}`)
+              }
+            }
+            else if(error.message){
+              this.errors.push('Something went wrong. Please try again')
+            }
+          })
+      }
+
+      console.log('hello')
+    }
+  }
 };
 </script>
 
