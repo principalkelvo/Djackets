805eee3a6b5362379d0842935e52cfd93b4e0ec4
principalkelvoe <you@example.com>
2021-11-14 03:31:11 +0300

    navbar as component

---
+++ djackets_vue/src/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="navbar is-dark">
+    <div class="navbar-brand">
+      <router-link to="/" class="navbar-item"
+        ><strong>Djackets</strong></router-link
+      >
+      <a
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbar-menu"
+        @click="showMobileMenu = !showMobileMenu"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span
+      ></a>
+    </div>
+    <div
+      class="navbar-menu"
+      id="navbar-menu"
+      v-bind:class="{ 'is-active': showMobileMenu }"
+    >
+      <div class="navbar-end">
+        <router-link to="/summer" class="navbar-item">Summer</router-link>
+        <router-link to="/winter" class="navbar-item">Winter</router-link>
+        <div class="navbar-item">
+          <div class="buttons">
+            <router-link to="/log-in" class="button is-light"
+              >Log In</router-link
+            >
+            <router-link to="/cart" class="button is-success">
+              <span class="icon">
+                <i class="fas fa-shopping-cart"></i>
+              </span>
+              <span>cart</span>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+    name:'Navbar'
+}
+</script>
