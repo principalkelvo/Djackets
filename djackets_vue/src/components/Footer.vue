a4dd248cb3d9a190bf0fe84a4e8550bf32b213e4
principalkelvoe <you@example.com>
2021-11-14 03:30:56 +0300

    created footer component

---
+++ djackets_vue/src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="footer">
+    <p class="has-text-centered">
+      Copyright <i class="fa fa-copyright" aria-hidden="true"></i> 2021
+    </p>
+  </footer>
+</template>
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
