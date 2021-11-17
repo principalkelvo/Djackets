91ed392990870202bf0f20da5093bcfc91dd6916
principalkelvoe <you@example.com>
2021-11-17 20:14:31 +0300

    create view product

--- djackets_django/product/views.py
+++ djackets_django/product/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.serializers import Serializer
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+
+class LatestProductList(APIView): #viewset to get latest products to show in frontend
+    def get(self,request, format= None):
+        products = Product.objects.all()[0:4] #this shows number of items to be shown in latest products
+        serializer =ProductSerializer(products, many=True)
+        return Response(serializer.data)
+        
 
 # Create your views here.
