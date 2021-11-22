49b03e4134c70e54118be9b9f55edaa7d2c5641e
principalkelvoe <you@example.com>
2021-11-22 22:51:04 +0300

    add product description and view

--- djackets_vue/src/views/Home.vue
+++ djackets_vue/src/views/Home.vue
@@ -30,7 +30,7 @@
           </figure>
           <h3 class="is-size-4">{{ product.name }}</h3>
           <p class="is-size-6 has-text-grey">${{ product.price }}</p>
-          view details
+          <router-link v-bind:to="product.get_absolute_url" class="buton is-dark mt-4">View details</router-link>
         </div>
       </div>
     </div>
