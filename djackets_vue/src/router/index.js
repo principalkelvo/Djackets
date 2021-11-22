94223ab49e2eafb713cbabb8aa2594ea1f8a7b8e
principalkelvoe <you@example.com>
2021-11-22 22:51:26 +0300

    added product to router

--- djackets_vue/src/router/index.js
+++ djackets_vue/src/router/index.js
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue'
 import SignUp from '../views/Signup.vue'
 import Login from '../views/Login.vue'
 import MyAccount from '../views/MyAccount.vue'
+import Product from '../views/Product.vue'
 
 const routes = [
   {
@@ -33,6 +34,14 @@ const routes = [
     path: '/myaccount',
     name: 'MyAccount',
     component: MyAccount,
+    meta:{
+      requireLogin: true
+    }
+  },
+  {
+    path: '/:category_slug/:product_slug',
+    name: 'Product',
+    component: Product,
     meta:{
       requireLogin: true
     }
