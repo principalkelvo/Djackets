7a900ebcb3438bf3e1403c80f820bde79e7aa5a6
principalkelvoe <you@example.com>
2021-11-23 19:50:15 +0300

    changed category slug

--- djackets_django/product/models.py
+++ djackets_django/product/models.py
@@ -20,7 +20,7 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    slug = models.SlugField
+    slug = models.SlugField()
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='uploads/', blank= True, null=True)
