4d780fe5def3b2f6ab554b5c6bf157bf321054f9
principalkelvoe <you@example.com>
2021-11-23 19:51:52 +0300

    view a product detail

--- djackets_vue/src/views/Home.vue
+++ djackets_vue/src/views/Home.vue
@@ -30,7 +30,9 @@
           </figure>
           <h3 class="is-size-4">{{ product.name }}</h3>
           <p class="is-size-6 has-text-grey">${{ product.price }}</p>
-          <router-link v-bind:to="product.get_absolute_url" class="buton is-dark mt-4">View details</router-link>
+
+          <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
+          
         </div>
       </div>
     </div>
@@ -53,6 +55,7 @@ export default {
   },
   methods:{
     async getLatestProducts(){
+      this.$store.commit('setIsLoading',true)
       await axios
         .get('/api/v1/latest-products/')
         .then(response=>{
@@ -61,6 +64,9 @@ export default {
         .catch(error=>{
           console.log(error)
         })
+
+
+        this.$store.commit('setIsLoading',false)
     }
   }
 };
