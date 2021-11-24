b911a92218a4b367bff2b0ee9069f9ce580a0f09
principalkelvoe <you@example.com>
2021-11-24 18:17:02 +0300

    search backend

--- djackets_django/product/views.py
+++ djackets_django/product/views.py
@@ -1,6 +1,8 @@
+from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import render
 from rest_framework import serializers
+from rest_framework.decorators import api_view
 from rest_framework.serializers import Serializer
 
 from rest_framework.views import APIView
@@ -40,5 +42,16 @@ class CategoryDetail(APIView):
         serializer= CategorySerializer(category)
         return Response(serializer.data)
 
+#add search functionality
+@api_view (['POST'])
+def search(request):
+    query= request.data.get('query', '')
 
+    if query:
+        products= Product.objects.filter(Q(name__icontains=query) | Q(description__icontains= query))
+        serializer= ProductSerializer(products, many= True)
+        return Response(serializer.data)
+    
+    else:
+        return Response({"products":[]})
 # Create your views here.
