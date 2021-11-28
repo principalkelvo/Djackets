4ccd099e7b06f853d17ec9256145d015b4b10932
principalkelvoe <you@example.com>
2021-11-28 15:45:28 +0300

    created checkout page and

--- djackets_vue/src/views/Checkout.vue
+++ djackets_vue/src/views/Checkout.vue
@@ -37,6 +37,83 @@
                     </tfoot>
                 </table>
             </div>
+            
+            <div class="column is-12 box">
+                <h2 class="subtitle">Shipping Details</h2>
+                <p class="has-text-grey mb-4">*All fields are required</p>
+
+                <div class="columns is-multiline">
+                    <div class="column is-6">
+
+                        <!--on the left-->
+                        <div class="field">
+                            <label>First name*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="first_name">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <label>Last name*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="last_name">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <label>E-mail*</label>
+                            <div class="control">
+                                <input type="email" class="input" v-model="email">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <label>Phone*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="phone">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!--right side-->
+                    <div class="column is-6">
+
+                        <div class="field">
+                            <label>Address*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="address">
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label>Zip Code*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="zipcode">
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label>Place*</label>
+                            <div class="control">
+                                <input type="text" class="input" v-model="place">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="notification is-danger mt-4" v-if="errors.length">
+                        <p v-for="error in errors"
+                            v-bind:key="error">{{error}}>
+                        </p>
+                    </div>
+
+                    <hr>
+
+                    <div id="card-element" class="mb-5"></div>
+                    <template v-if="cartTotalLength">
+                        
+                        <hr>
+
+                        <button class="button is-dark" @click="submitForm">Pay with Stripe</button>
+                    </template>
+                </div>
+            </div>
         </div>
   </div>
 </template>
@@ -56,6 +133,9 @@ export default {
             last_name:'',
             email:'',
             phone:'',
+            address:'',
+            zipcode:'',
+            place:'',
             errors:[]
         }
     },
@@ -66,17 +146,42 @@ export default {
     methods:{
         getItemTotal(item){
             return item.quantity*item.product.price
+        },
+        submitForm(){
+            this.errors =[]
+
+            if(this.first_name===''){
+                this.errors.push('The first name field is missing!')
+            }
+            if(this.last_name===''){
+                this.errors.push('The last name field is missing!')
+            }
+            if(this.email===''){
+                this.errors.push('The email field is missing!')
+            }
+            if(this.phone===''){
+                this.errors.push('The phone field is missing!')
+            }
+            if(this.address===''){
+                this.errors.push('The address field is missing!')
+            }
+            if(this.zipcode===''){
+                this.errors.push('The zipcode field is missing!')
+            }
+            if(this.place===''){
+                this.errors.push('The place field is missing!')
+            }
         }
     },
     computed:{
-        cartTotalLength(){
-            return this.cart.items.reduce((acc,curVal)=>{
-                return acc+= curVal.quantity
-            },0)
-        },
         cartTotalPrice(){
             return this.cart.items.reduce((acc,curVal)=>{
                 return acc+= curVal.product.price*curVal.quantity
+            },0)
+        },
+        cartTotalLength(){
+            return this.cart.items.reduce((acc,curVal)=>{
+                return acc+= curVal.quantity
             },0)
         }
     }
