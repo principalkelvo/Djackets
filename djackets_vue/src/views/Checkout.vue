38ebab59f119caa6d466e39be4295d82078b3d23
principalkelvoe <you@example.com>
2021-11-28 21:27:43 +0300

    stripe and checkout of items

--- djackets_vue/src/views/Checkout.vue
+++ djackets_vue/src/views/Checkout.vue
@@ -142,6 +142,13 @@ export default {
     mounted(){
         document.title='Checkout | Djackets'
         this.cart= this.$store.state.cart
+
+        if(this.cartTotalLength>0){
+            this.stripe= stripe('pk_test_51JCGIbJmktMF91UU0SQVvoPAoy4KVZpBqcWj20xXV1MwIxzZySFeuSOiI93Cbgo6vRpCHftKCcMah0s25AzvkPOi00emNTeTu3')
+            const elements= this.stripe.elements();
+            this.card= elements.create('card',{hidePostalCode:true})
+            this.card.mount('#card-element')
+        }
     },
     methods:{
         getItemTotal(item){
@@ -171,6 +178,56 @@ export default {
             if(this.place===''){
                 this.errors.push('The place field is missing!')
             }
+            if(!this.errors.length){
+                this.$store.commit('setIsLoading',true)
+                this.stripe.createToken(this.card).then(result=>{
+                    if(result.error){
+                        this.$store.commit('setIsLoading',false)
+                        this.errors.push('Something went wrong with Stripe. Please try again')
+                        console.log(results.error.message)
+                    }
+                    else{
+                        this.stripeTokenHandler(result.token)
+                    }
+                })
+            }
+        },
+        async stripeTokenHandler(token){
+            const items=[]
+            for(let i=0; i<this.cart.items.length;i++){
+                const item= this.cart.items[i]
+                const obj= {
+                    product: item.product.id,
+                    quantity:item.quantity,
+                    price:item.product.price* item.quantity
+                }
+                items.push(obj)
+            }
+
+            const data={
+                'first_name':this.first_name,
+                'last_name':this.last_name,
+                'email':this.email,
+                'address':this.address,
+                'zipcode':this.zipcode,
+                'place':this.place,
+                'phone':this.phone,
+                'items':this.items,
+                'stripe_token':token.id
+            }
+
+            await axios
+                .post('/api/v1/checkout/', data)
+                .then(response=>{
+                    this.$store.commit('clearCart')
+                    this.$router.push('/cart/success')
+                })
+                .catch(error=>{
+                    this.errors.push('Something went wrong. Please try again')
+                    console.log(error)
+                })
+            
+            this.$store.commit('setIsLoading',false)
         }
     },
     computed:{
