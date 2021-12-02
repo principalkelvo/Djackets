06d0752b169c091cc8ee637d5418b2ccb4d18c42
principalkelvoe <you@example.com>
2021-12-02 10:22:30 +0300

    clear cart after success not working

--- djackets_vue/src/store/index.js
+++ djackets_vue/src/store/index.js
@@ -49,6 +49,13 @@ export default createStore({
     removeToken(state){
       state.token=''
       state.isAuthenticated= false
+    },
+    clearCart(state){
+      state.cart= {
+        items:[]
+      }
+
+      localStorage.setItem('cart',JSON.stringify(state.cart))
     }
   },
   actions: {
