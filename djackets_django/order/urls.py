2923d11afb365afda6467dcadfab06303263ac79
principalkelvoe <you@example.com>
2021-12-02 13:28:53 +0300

    Show orders on the profile page

--- djackets_django/order/urls.py
+++ djackets_django/order/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from order import views
 
 urlpatterns = [
-    path('checkout/', views.checkout)
+    path('checkout/', views.checkout),
+    path('orders/', views.OrdersList.as_view()),
 ]
--- djackets_django/order/views.py
+++ djackets_django/order/views.py
@@ -12,7 +12,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from stripe.api_resources import charge, source
 from .models import Order, OrderItem
-from .serializers import OrderSerializer
+from .serializers import OrderSerializer,MyOrderSerializer
 
 @api_view(['POST'])
 @authentication_classes([authentication.TokenAuthentication])
@@ -42,4 +42,13 @@ def checkout(request):
 
     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
+class OrdersList(APIView):
+    authentication_classes=[authentication.TokenAuthentication]
+    permission_classes=[permissions.IsAuthenticated]
+
+    def get(self,request,format=None):
+        orders= Order.objects.filter(user=request.user)
+        serializer= MyOrderSerializer(orders,many=True)
+
+        return Response(serializer.data)
 # Create your views here.
