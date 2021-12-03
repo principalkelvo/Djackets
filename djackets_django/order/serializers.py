31796f7b3867382c142c5f9c55fb3abb434105f0
principalkelvoe <you@example.com>
2021-12-03 09:30:38 +0300

    show orders serializer

--- djackets_django/order/serializers.py
+++ djackets_django/order/serializers.py
@@ -3,6 +3,34 @@ from rest_framework import serializers
 from .models import Order, OrderItem
 from product.serializers import ProductSerializer
 
+class MyOrderItemSerializer(serializers.ModelSerializer):
+    product= ProductSerializer()
+    class Meta:
+        model= OrderItem
+        fields=(
+            "price",
+            "product",
+            "quantity",
+        )
+
+class MyOrderSerializer(serializers.ModelSerializer):
+    items=MyOrderItemSerializer(many=True)
+    class Meta:
+        model= Order
+        fields=(
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "address",
+            "zipcode",
+            "place",
+            "phone",
+            "stripe_token",
+            "items",
+            "paid_amount",
+        )
+
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model= OrderItem
@@ -38,33 +66,6 @@ class OrderSerializer(serializers.ModelSerializer):
         
         return order
 
-class MyOrderItemSerializer(serializers.ModelSerializer):
-    product= ProductSerializer
-    class Meta:
-        model= OrderItem
-        fields=(
-            "price",
-            "product",
-            "quantity",
-        )
-
-class MyOrderSerializer(serializers.ModelSerializer):
-    items=MyOrderItemSerializer(many=True)
-    class Meta:
-        model= Order
-        fields=(
-            "id",
-            "first_name",
-            "last_name",
-            "email",
-            "address",
-            "zipcode",
-            "place",
-            "phone",
-            "stripe_token",
-            "items",
-            "paid_amount",
-        )
 
     def create(self, validated_data):
         items_data=validated_data.pop('items')
